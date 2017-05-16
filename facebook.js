ee3808af7a8ad5b34fc40e70e80003e8570bb797
alu0100885453 <alu0100885453@ull.edu.es>
2017-05-16 19:35:26 +0100

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -59,10 +59,10 @@ function FBBtnLogout(){
  function FBLogin()
  {
    var status = document.getElementById("status");
-	 console.log('status');
+	 console.log(status);
    FB.getLoginStatus(function(response) { 
    //Conectado
-	   console.log('response.status');
+	   console.log(response.status);
   if (response.status === 'connected') {
 	window.location.href = url_after_login;
   } 
